94753746b82e2c255cf10831f23ab290d3eb2ff0
urs-santoshh <santosh18timalsina@gmail.com>
2023-06-28 22:10:21 +0545

    defined all the models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Book, Transaction, Burrowing, Report
 
 # Register your models here.
 
-admin.site.register(Student)
+admin.site.register([Student, Book, Transaction, Burrowing, Report])
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
 
@@ -23,6 +24,56 @@ class Student(models.Model):
         return f"{prefix}{count + 1:04}"
 
     def __str__(self):
-        return f"Student: {self.fullname}"
+        return f"Student: {self.fullName}"
+    
+class BookStatus(models.TextChoices):
+    AVAILABLE = 'A', 'Available'
+    LOANED = 'L', 'Loaned'
+    RETURNED = 'R', 'Returned'
+    DUE = 'D', 'Due Date Passed'
+    OVERDUE = 'O', 'Overdue'
+    BORROWER_NOTIFIED = 'B', 'Borrower Notified'
+    RECEIVED = 'I', 'Received from Library'
+    DISCARDED = 'X', 'Discarded by Librarian'
+    SOLD = 'S', 'Sold to Customer'
+    INCORRECTLY_RECEIVED = 'C', 'Incorrectly Received'
+    MISSING = 'M', 'Missing in the library'
+    NOT_AVAILABLE = 'N', 'Not Available at this time.'
+    UNKNOWN = 'U', 'Unknown Status'
+    
+class Book(models.Model):
+    bookId = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=70, unique=True)
+    status = models.CharField(max_length=1, choices=BookStatus.choices, default=BookStatus.AVAILABLE)
+    isbn = models.CharField(max_length=(50), unique=True)
+
+    def __str__(self):
+        return f"Book: {self.title}"
+
+class Transaction(models.Model):
+    transactionId = models.AutoField(primary_key=True)
+    studetnId = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+
+class Burrowing(models.Model):
+    burrowingId = models.AutoField(primary_key=True)
+    bookId = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class Report(models.Model):
+    reportId = models.AutoField(primary_key=True)
+    transactionId = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    burrowingId = models.ForeignKey(Burrowing, on_delete=models.CASCADE)
+    transactionDate = models.DateField(auto_now_add=True, unique=True)
+    returnDate = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.reportId and not self.returnDate:
+            self.returnDate = self.transactionDate + timedelta(days=14)
+        return super().save(*args, **kwargs)
+
+
+
+
     
 
